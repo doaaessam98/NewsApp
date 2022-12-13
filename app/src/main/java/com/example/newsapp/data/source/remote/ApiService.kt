55e44a159948b577e73e22dc6098bf648331f74e
Doaa Essam <doaaessam2021@gmail.com>
2022-12-13 21:42:27 +0200

    add models and  function in api that get  news headlines

--- app/src/main/java/com/example/newsapp/data/source/remote/ApiService.kt
+++ app/src/main/java/com/example/newsapp/data/source/remote/ApiService.kt
@@ -1,10 +1,20 @@
 package com.example.newsapp.data.source.remote
 
+import com.example.newsapp.models.NewsResponse
+import com.example.newsapp.utils.Constants
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 
 interface ApiService {
 
-
-
+    @GET("top-headlines")
+    suspend fun getNews(
+        @Query("country") country: String = Constants.DEFAULT_LANG,
+        @Query("category") category: String?,
+        @Query("apiKey") api: String = Constants.BASE_URL
+    ): Response<NewsResponse>
 
 
 
--- app/src/main/java/com/example/newsapp/utils/Constants.kt
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -1,6 +1,7 @@
 package com.example.newsapp.utils
 
 object Constants {
+    const val DEFAULT_LANG: String ="US"
     const val BASE_URL = " https://newsapi.org/v2/"
 
 ///top-headlines?country=us&apiKey=5ab7a9ebb21b4bc79493f452de61b602
